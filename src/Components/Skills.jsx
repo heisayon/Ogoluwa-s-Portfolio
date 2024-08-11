2f0db5f03f30153dd056b662acd5a4ed5af7d969
Zion <heisayon@gmail.com>
2024-08-11 21:20:10 +0100

    skills added

---
+++ src/Components/Skills.jsx
@@ -0,0 +1,53 @@
+function Skills() {
+  return (
+    <div className="flex gap-7 flex-wrap justify-center">
+      <div className="border-2 max-w-[500px] text-center py-5 px-7 rounded-md">
+        <h1 className="text-2xl font-bold mb-2">GRAPHIC SKILLS</h1>
+
+        <p>
+          I create visual concepts to communicate ideas that inspire, inform and
+          captivate consumers. I use digital illustration and photo editing
+          software to create designs. ( Adobe Photoshop, Canva, Corel Draw and
+          Pixellab.) I review designs for errors before printing or publishing
+          them and ensure projects are completed with high quality and on
+          schedule.
+        </p>
+      </div>
+      <div className="border-2 max-w-[500px] text-center py-5 px-7 rounded-md">
+        <h1 className="text-2xl font-bold mb-2">TECHNICAL SKILLS</h1>
+
+        <p>
+          Proficient in tools like Discord, Telegram, and various social media
+          and crypto platforms. Basic understanding of blockchain technology and
+          smart contracts. Basic knowledge of embedded systems and robotics.
+          Core knowledge of Cybersecurity Basic knowledge of programming.
+        </p>
+      </div>
+      <div className="border-2 max-w-[500px] text-center py-5 px-7 rounded-md">
+        <h1 className="text-2xl font-bold mb-2">COLLAB SKILLS</h1>
+        <p>
+          Partnering with influencers and key stakeholders to promote projects.
+          Volunteering in tech events to increase exposure and learn new
+          technologies.
+        </p>
+      </div>
+      <div className="border-2 max-w-[500px] text-center py-5 px-7 rounded-md">
+        <h1 className="text-2xl font-bold mb-2">SHILLING</h1>
+        <p>
+          Creating and executing marketing campaigns for Web3 projects.
+          Leveraging social media to increase visibility.
+        </p>
+      </div>
+      <div className="border-2 max-w-[500px] text-center py-5 px-7 rounded-md">
+        <h1 className="text-2xl font-bold mb-2">COMMUNITY MANAGEMENT SKILLS</h1>
+
+        <p>
+          Overseeing online communities on platforms (Discord and Telegram).
+          Ensuring activeness and involvement.
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default Skills;
--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,3 +1,4 @@
+import Skills from "../Components/Skills";
 import Xpfp from "/assets/images/twitterpfp.jpg";
 
 function About() {
@@ -14,20 +15,27 @@ function About() {
               Hey there, I&rsquo;m Ogoluwa Odejimi. With over 3 years as a
               passionate graphic designer, creating mind wobbling and engaging
               visuals and 2 years in the Web3 space, I have worked with
-              <b> Chain2055</b> as a graphic designer as well as moderating crypto
-              project, communities and collaborated on innovative projects.{" "}
+              <b> Chain2055</b> as a graphic designer as well as moderating
+              crypto project, communities and collaborated on innovative
+              projects.{" "}
             </h1>
 
             <h1>
               I am currently pursuing a degree in Computer Science at University
               of Uyo. I have a certification in Embedded Systems and Robotics
-              from <b>LASkill </b> and have served as a community moderator, collab
-              manager and shiller on both Discord and Telegram. Send a message
-              and let&rsquo;s get creative.
+              from <b>LASkill </b> and have served as a community moderator,
+              collab manager and shiller on both Discord and Telegram. Send a
+              message and let&rsquo;s get creative.
             </h1>
             <h1>The Site is yours, Explore.</h1>
           </section>
         </div>
+      </div>
+      <div className="mt-20">
+      <h1 className="flex justify-center text-4xl font-semibold">Skills</h1>
+      <section className="flex justify-center pt-10 w-full">
+        <Skills />
+      </section>
       </div>
     </>
   );
