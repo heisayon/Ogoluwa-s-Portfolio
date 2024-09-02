e5e236cd6c4f5425b376aff86ced3071ea65f143
heisayon <heisayon@gmail.com>
2024-09-03 00:02:49 +0100

    images removed

--- src/constants/index.js
+++ src/constants/index.js
@@ -20,10 +20,6 @@ export const images = [
     alt: "My Past Projects",
   },
   {
-    src: "/assets/images/Carousel Images/photo_2024-07-28_23-46-44.jpg",
-    alt: "My Past Projects",
-  },
-  {
     src: "/assets/images/Carousel Images/photo_2024-07-28_23-47-00.jpg",
     alt: "My Past Projects",
   },
@@ -31,8 +27,5 @@ export const images = [
     src: "/assets/images/Carousel Images/photo_2024-07-28_23-47-14.jpg",
     alt: "My Past Projects",
   },
-  {
-    src: "/assets/images/Carousel Images/photo_2024-07-28_23-47-24.jpg",
-    alt: "My Past Projects",
-  },
+
 ];
